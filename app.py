ed58f7309a53598c1b86276bd482b94eeedaa9ef
aythamilorenzo <aythamilorenzo06@gmail.com>
2025-09-13 14:26:16 +0100

    Add unit, weight_lifted, and reps variables; implement rmcalculator function and display message

--- app.py
+++ app.py
@@ -2,8 +2,22 @@ import streamlit as st
 
 def main():
     
-    st.markdown("<h1 style='align-center;'>1RM Bench Press Calculator</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>1RM Bench Press Calculator</h1>", unsafe_allow_html=True)
     
+    unidad = st.selectbox(
+        '',
+        ['kg', 'lb'])
+    
+    weight_lifted = st.number_input("Lifted", 20, 10000000000)
+    reps = st.number_input("Reps", 1, 10000000000)
+    
+    def rmcalculator(weight_lifted, reps):
+        if reps == 1:
+            return weight_lifted
+        else:
+            return round((weight_lifted * (1 + 0.0333 * reps)), 0)
+    
+    st.success(f"Your 1RM is: {rmcalculator(weight_lifted, reps)} {unidad}")
     
 if __name__ == '__main__':
     main()
