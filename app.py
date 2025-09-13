8ebf835353ec7ee6fb695370a3cfa6d197428040
aythamilorenzo <aythamilorenzo06@gmail.com>
2025-09-13 13:23:06 +0100

    Main function - Dark theme default config folder - Added a .gitignore folder

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    
+    st.markdown("<h1 style='align-center;'>1RM Bench Press Calculator</h1>", unsafe_allow_html=True)
+    
+    
+if __name__ == '__main__':
+    main()
